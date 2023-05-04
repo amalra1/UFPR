660c97179600a3ec646cfa44402c4b4e86109d06
PEDRO AMARAL CHAPELIN <pac20@inf.ufpr.br>
2023-05-04 18:55:58 -0300

    a

--- Otimização/lp_solve.c
+++ Otimização/lp_solve.c
@@ -1,4 +1,23 @@
-#include <stdio.h>;
-#include <stdlib.h>;
-#include "lp_lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "lp_solve_5.5/lp_lib.h"
+
+
+int main () 
+{
+    lprec *lp;
+    
+    /* Read LP model */
+    lp = read_LP("model.lp", NORMAL, "test model");
+    
+    if (lp == NULL)
+    {
+        fprintf(stderr, "Unable to read model\n");
+        return(1);
+    }
+
+  delete_lp(lp);
+
+    return 0;
+}
 
