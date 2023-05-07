b2769bb0f42093437ea7ed7a9fde93f6ad9770ae
Pedro <amaral1@ufpr.br>
2023-05-07 14:47:49 -0300

    funcionando

--- Otimização/DotLPMaker.c
+++ Otimização/DotLPMaker.c
@@ -9,6 +9,8 @@ int main ()
   FILE* arqLP;
   tabela_t *tabela = malloc(sizeof(tabela_t));
   char linha[TAM_LINHA];
+  int i, j;
+  float aux;
 
 
   // ETAPA 1 - COLETA DOS DADOS
@@ -24,39 +26,59 @@ int main ()
 
   le_quantsPorL(linha, tabela);
 
-  imprime_dados(tabela);
-
   // ETAPA 2 - ESCRITA EM UM ARQUIVO NO MODELO ACEITO PELO LP_SOLVE
 
+  strcpy(linha, "");
   arqLP = fopen("entrada.lp", "w");
 
-
-
-
-
-
-
-
-
-  /*arqIN = fopen("entrada.in", "r");
-
-  // Teste se o arquivo abre
-  if (arqIN == NULL)
+  // Testa se o arquivo abre
+  if (!arqLP)
   {
-    printf("Erro ao abrir arquivo de entrada\n");
+    printf("Impossivel abrir arquivo\n");
     exit(1);
   }
 
-  fclose(arqIN);*/
+  // Escreve funcao objetivo
+  i = 0;
+  fprintf(arqLP, "max: ");
+  while (i < tabela->quantN - 1)
+  {
+    aux = 0;
 
-  // for (i = 0; i < quantM; i++)
-  // {
-  //   printf("composto [%d]\n", i);
-  //   printf("custo = %f\n", m[i].custo);
-  //   printf("limite = %d\n\n", m[i].limite);
-  // }
+    for (j = 0; j < tabela->quantM; j++)
+      aux += tabela->c[i][j] * tabela->m[j].custo;
 
-  //system("lp_solve -S3 < model.lp");
+    fprintf(arqLP, "%.1fx%d + ", tabela->n[i].valor - aux, i + 1);
+    i++;
+  }
+
+  // Faz o mesmo para o ultimo composto
+  aux = 0;
+  for (j = 0; j < tabela->quantM; j++)
+      aux += tabela->c[i][j] * tabela->m[j].custo;
+
+  fprintf(arqLP, "%.1fx%d;\n\n", tabela->n[i].valor - aux, i + 1);
+  
+
+  // Escreve as restricoes 
+  j = 0;
+  while (j < tabela->quantM)
+  {
+    i = 0;
+    while (i < tabela->quantN - 1)
+    {
+      fprintf(arqLP, "%.1fx%d + ", tabela->c[i][j], i + 1);
+      i++;
+    }
+
+    fprintf(arqLP, "%.1fx%d <= %d;\n", tabela->c[i][j], i + 1, tabela->m[j].limite);
+    j++;
+  }
+
+  // Restricoes de nao-negatividade
+  i = 0;
+  for (i = 0; i < tabela->quantN; i++)
+    fprintf(arqLP, "x%d >= 0;\n", i + 1);
 
 
   fclose(arqLP);
