6b91e9b863bba79a7dd558e22534cc77dcda5de5
Pedro <amaral1@ufpr.br>
2023-05-17 22:58:52 -0300

    a

--- Programação Web/visualizeitor/visualizeitor.js
+++ Programação Web/visualizeitor/visualizeitor.js
@@ -15,14 +15,33 @@ class SubjectData
 // Array to store all subject datas from a student
 var allSubjectsData = [];
 
+var subjectCodes = [
+  'CI069', 'CI084', 'CI085', 'CI086', 'CI087', 'CI088', 'CI089', 'CI090',
+  'CI091', 'CI092', 'CI093', 'CI094', 'CI095', 'CI097', 'CI167', 'CI168',
+  'CI169', 'CI170', 'CI171', 'CI172', 'CI173', 'CI174', 'CI204', 'CI205',
+  'CI214', 'CI301', 'CI302', 'CI303', 'CI304', 'CI305', 'CI306', 'CI309',
+  'CI310', 'CI311', 'CI312', 'CI313', 'CI314', 'CI315', 'CI316', 'CI317',
+  'CI318', 'CI320', 'CI321', 'CI337', 'CI338', 'CI339', 'CI340', 'CI350',
+  'CI351', 'CI355', 'CI358', 'CI359', 'CI360', 'CI361', 'CI362', 'CI363',
+  'CI364', 'CI365', 'CI366', 'CI367', 'CI394', 'CI395', 'CI396', 'ET082',
+  'CE211', 'CM043', 'HL077', 'SA017', 'SC003', 'SC202', 'SC203',
+  'CI068', 'CI055', 'CM046', 'CM045', 'CM201',
+  'CI210', 'CI056', 'CI067', 'CM005', 'CM202',
+  'CI212', 'CI057', 'CI064', 'CI237', 'CI166',
+  'CI215', 'CI062', 'CE003', 'CI058', 'CI164',
+  'CI162', 'CI065', 'CI059', 'CI061', 'SA214',
+  'CI163', 'CI165', 'CI209', 'CI218', 'CI220',
+  'CI221', 'CI211'
+];
+
 // Dictionary to store subject data
-let subjectDataObj = {};
+var subjectDataObj = {};
 
 function loadData()
 {
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () 
-    {
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () 
+  {
     // Request finished and response 
     // is ready and Status is "OK"
     if (this.readyState == 4 && this.status == 200)
@@ -43,10 +62,20 @@ function collectData()
   let grrAlunoInput = input.value;
   let alunos = xmlStr.getElementsByTagName("ALUNO");
   let grrAluno, subCode, situation, grade;
+  let i, element, a;
+
+  for (i = 0; i < subjectCodes.length; i++)
+  {
+    a = subjectCodes[i];
+    element = document.getElementById(a);
+
+    element.style.backgroundColor = "white";
+  }
+
 
   console.log(grrAlunoInput);
 
-  for (var i = 0; i < alunos.length; i++) 
+  for (i = 0; i < alunos.length; i++) 
   {
     grrAluno = alunos[i].getElementsByTagName("MATR_ALUNO")[0].childNodes[0].nodeValue;
 
@@ -74,17 +103,60 @@ function collectData()
   }
 
   // teste
+  /*
   for (i = 0; i < allSubjectsData.length; i++)
   {
     console.log("COD: " + allSubjectsData[i].subjectCode);
     console.log("SITUACAO: " + allSubjectsData[i].situation);
     console.log("MEDIA FINAL: " + allSubjectsData[i].grade);
   }
+  */
+}
 
-  // Agora para achar a materia mais recente eh so ler o vetor ao contrario
+// Function that goes over the sorted array of subjects and gets
+// the last record of a given subject code (the last record is 
+// the one that matters for the box colors)
+function getLastRecordOfSubject(subCode) 
+{
+  let lastRecord = null;
+  let i;
+  
+  for (i = allSubjectsData.length - 1; i >= 0; i--) 
+  {
+    if (allSubjectsData[i].subjectCode === subCode) 
+    {
+      lastRecord = allSubjectsData[i];
+      console.log(allSubjectsData[i].subjectCode);
+      console.log(allSubjectsData[i].situation);
+      break;
+    }
+  }
+  return lastRecord;
 }
 
 function updateGrade()
 {
+  // Agora para achar a materia mais recente eh so ler o vetor ao contrario
+  let i, subCode, lastRecord, element;
 
+  for (i = 0; i < subjectCodes.length; i++) 
+  {
+    subCode = subjectCodes[i];
+    lastRecord = getLastRecordOfSubject(subCode);
+
+    element = document.getElementById(subCode);
+
+    if (lastRecord && lastRecord.situation === "Aprovado") 
+      element.style.backgroundColor = "green";
+    else if (lastRecord && lastRecord.situation === "Reprovado por nota")
+      element.style.backgroundColor = "red";
+    else if (lastRecord && lastRecord.situation === "Reprovado por Frequência")
+      element.style.backgroundColor = "red";
+    else if (lastRecord && lastRecord.situation === "Matrícula")
+      element.style.backgroundColor = "blue";
+    else if (lastRecord && lastRecord.situation === "Equivalência de Disciplina")
+      element.style.backgroundColor = "yellow";
+    else
+      element.style.backgroundColor = "white";
+  }
 }
